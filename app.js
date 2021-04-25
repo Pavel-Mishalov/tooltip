db644b4479fb9e052ee1e2129f37a07b7d8cb754
Paha <pahamihey@gmail.com>
2021-04-25 17:57:10 +0300

    Init

---
+++ app.js
@@ -0,0 +1,18 @@
+// Пример внешнего вида tooltip по ссылке
+// https://overcoder.net/img/1/4/38/33701.png
+
+class Tooltip {
+	/**
+	  * @param {NodeElement} element элемент к которому необходимо создать tooltip
+	  * @param {string}      text    текст содержимого tooltip
+	  */
+	constructor(element, text) {}
+
+	// Инициализация работы данного tooltip
+	// при наведении на элемент он должен появляться
+	init() {}
+
+	// Остановка работы данного tooltip
+	// при наведении на элемент больше не выполнять никаких действий
+	destroy() {}
+}
